05f3396fa39fd0b0f9d0abdff4162c36cd6be174
Rachel2070 <rachelwe939@gmail.com>
2024-03-04 17:02:08 +0200

    connect controller to model

--- userController.js
+++ userController.js
@@ -1,17 +1,18 @@
+const userModel = require('./data/model')
 function createUser(){
-
+    userModel.User.createUser()
 }
 
 function updateUser(){
-
+    userModel.User.updateUser()
 }
 
 function deleteUser(){
-
+    userModel.User.delteUser()
 }
 
 function getUserById(){
-
+    userModel.User.getUserById()
 }
 
 module.exports={createUser, updateUser, deleteUser, getUserById}
