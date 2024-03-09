8481b02da2fd7c44b696ba0af297abe459c9c7c9
Rachel2070 <rachelwe939@gmail.com>
2024-03-09 20:22:54 +0200

    adding 404 Error test

--- tests/user.controller.updateUser.test.js
+++ tests/user.controller.updateUser.test.js
@@ -1,15 +1,15 @@
-const {updateUser} = require('../controllers/user.controller');
+const { updateUser } = require('../controllers/user.controller');
 const userService = require('../data/service');
 
-jest.mock('../data/service', ()=>({
-    updateUser : jest.fn()
+jest.mock('../data/service', () => ({
+    updateUser: jest.fn()
 }));
 
-describe('Update user function', ()=>{
-    it('should handle updating user', async()=>{
-        const req = {params:{id : 1}, body:{fullName:'Updated Name'}};
-        const res = {status:jest.fn().mockReturnThis(), send:jest.fn()};
-        
+describe('Update user function', () => {
+    it('should handle updating user', async () => {
+        const req = { params: { id: 1 }, body: { fullName: 'Updated Name' } };
+        const res = { status: jest.fn().mockReturnThis(), send: jest.fn() };
+
         userService.updateUser.mockResolvedValue(req.body);
 
         await updateUser(req, res)
@@ -19,14 +19,27 @@ describe('Update user function', ()=>{
         expect(res.send).toHaveBeenCalledWith(`User updated: ${req.body}`);
     });
 
-    it('should handle updating errors', async()=>{
-        const req = {params:{id : 1}, body:{fullName:'Updated Name'}};
-        const res = {status:jest.fn().mockReturnThis(), send:jest.fn()};
-        
+    it('should handle 404 errors while updating', async () => {
+        const req = { params: { id: null }, body: { fullName: 'Updated Name' } };
+        const res = { status: jest.fn().mockReturnThis(), send: jest.fn() };
+
+        userService.updateUser.mockResolvedValue(null);
+
+        await updateUser(req, res);
+
+        expect(userService.updateUser).toHaveBeenCalledWith(req.params.id, req.body);
+        expect(res.status).toHaveBeenCalledWith(404);
+        expect(res.send).toHaveBeenCalledWith(`This id does not exist`);
+    });
+
+    it('should handle 500 errors while updating', async () => {
+        const req = { params: { id: 1 }, body: { fullName: 'Updated Name' } };
+        const res = { status: jest.fn().mockReturnThis(), send: jest.fn() };
         const errorMessage = 'Internal Server Error';
+
         userService.updateUser.mockRejectedValue(new Error(errorMessage));
-  
-        await updateUser(req, res)
+
+        await updateUser(req, res);
 
         expect(userService.updateUser).toHaveBeenCalledWith(req.params.id, req.body);
         expect(res.status).toHaveBeenCalledWith(500);
